71a8bf88c60225b405e02d7bb62adc8703dd15cd
hiraaaaaa <hirafayyaz422@gmail.com>
2018-07-23 05:14:08 +0500

    erp

---
+++ hira.erp/hir.erp/Form3.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace zunaiberp
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oleDbCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.SuspendLayout();
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form3";
+            this.Text = "oledb";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.OleDb.OleDbCommand oleDbCommand1;
+
+    }
+}
